f1c26a5052e54f3cc2b5c8f9c3c9811ad55b3801
Sanil14 <saniljalan14@gmail.com>
2022-01-23 10:01:39 +0000

    fix webpack issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+
     return (
         <>
-        <input className="rounded h-16 w-1/2 text-4xl p-4" placeholder="Submit A Post..."/>
+            <input className="rounded h-16 w-1/2 text-4xl p-4" placeholder="Submit A Post..." />
         </>
     )
 }
