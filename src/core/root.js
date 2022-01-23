b866acdc4a019747cd8902c35fc1c7dcb77b155d
SukritTM <sukrittmani@gmail.com>
2022-01-23 08:37:52 +0530

    implement tailwind
    Co-authored-by: Sanil Jalan <Sanil14@users.noreply.github.com>

--- src/core/root.js
+++ src/core/root.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "../pages/Home";
+import Navbar from "./navbar";
+import "../styles/global.css"
 
 const Root = () => {
     return (
         <Router>
-            {/* Navbar Here */}
+            <Navbar />
             <Routes>
                 <Route exact path="/" element={<Home />}></Route>
             </Routes>
