07a47c6a9cef4149fd788dcb4ca5babd41e243a0
SukritTM <sukrittmani@gmail.com>
2022-01-23 21:21:27 +0530

    cors support

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,12 @@ from flask import Flask, Blueprint
 from flask_restful import Api, Resource, url_for
 from resources.post import Post
 from resources.user import User
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'oqUsuZlT8mGUPOqViOWDtnSAv_muz5fJIc8zv8beww0'
 api = Api(app)
+CORS(app)
 
 api.add_resource(User, '/user')
 api.add_resource(Post, '/post')
